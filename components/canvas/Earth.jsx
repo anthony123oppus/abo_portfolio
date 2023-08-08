4eec7853d040dea8332d210f30cc20fa76bc5103
anthony123oppus <anthonyoppus2000@gmail.com>
2023-08-08 11:27:27 +0800

    fixexed

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 // import {} from '../../public/planet/scene.gltf';
 
 const Earth = () => {
-  const earth = useGLTF("../planet/scene.gltf");
+  const earth = useGLTF("../../planet/scene.gltf");
 
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
