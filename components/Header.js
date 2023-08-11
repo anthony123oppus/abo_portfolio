9c46f6598cdd548eedaeb45a5ac1526574fb966d
anthony123oppus <anthonyoppus2000@gmail.com>
2023-08-11 16:03:24 +0800

    fised

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         <div className='flex flex-col lg:flex-row justify-between items-center gap-y-5 py-8'>
           {/* logo */}
           <Link href={'/'} className='flex items-center'> 
-          <Image src={'/anthony.svg'} width={90} height={90} alt='' priority={true} className='mt-3 mr-2 xl:mr-3 xl:mt-20 flex'/>
+          <Image src={'/anthony.svg'} width={60} height={60} alt='' priority={true} className='mt-3 mr-2 xl:mr-3 xl:mt-20 xl:h-[90px] xl:w-[90px] flex'/>
             <div className='flex flex-col'><h1 className='mt-3 text-2xl xl:text-4xl font-bold xl:mt-20'>Anthony</h1>
             <h2 className='-mt-2 xl:text-[20px] font-bold'>Oppus</h2>
             </div>
